02e2091d965986b4da29d606fd80f5cf92f3699d
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-06-06 20:12:42 -0400

    Add mongo/mongoose dependencies & cleanup template files

--- routes/api.js
+++ routes/api.js
@@ -11,37 +11,42 @@
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      console.log(req.body)
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+      console.log(req.body)
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
+      console.log(req.body)
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      console.log(req.body)
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
+      console.log(req.body)
+      console.log(req.params)
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
+
 };
